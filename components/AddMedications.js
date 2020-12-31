52aa5d794ee20c586762c4b084581079a73d7c8f
Susan Crayne <susancrayne@yahoo.com>
2020-12-31 13:53:20 -0500

    changed dummy data

--- components/AddMedications.js
+++ components/AddMedications.js
@@ -12,35 +12,15 @@ state = {
   items: [
 	{
 		id: 1,
-		name: 'Javascript'
+		name: 'abcxy'
 	},
 	{
 		id: 2,
-		name: 'Java'
+		name: 'abcxz'
 	},
 	{
 		id: 3,
-		name: 'Ruby'
-	},
-	{
-		id: 4,
-		name: 'React Native'
-	},
-	{
-		id: 5,
-		name: 'PHP'
-	},
-	{
-		id: 6,
-		name: 'Python'
-	},
-	{
-		id: 7,
-		name: 'Go'
-	},
-	{
-		id: 8,
-		name: 'Swift'
+		name: 'abcyz'
 	}
 ]
 }
@@ -90,7 +70,6 @@ function AddMedicationsDropdown(){
   return (
 
   <View>
-
 
     <Searchbar style={{maxHeight: 50, width: '80%', marginBottom: 0}}
       placeholder="Search"
