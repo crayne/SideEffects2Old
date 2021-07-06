4515ed3444aa339ff2bbc294068c08380f1820b8
Susan Crayne <susancrayne@yahoo.com>
2021-07-06 11:26:25 -0400

    changed http to https for release

--- components/SetHost.js
+++ components/SetHost.js
@@ -8,7 +8,7 @@ var localHost =  "http://192.168.1.228:8888/sideEffectsNewData";
 //This is for a connected Android phone
 //localHost =  "http://localhost:8888/sideEffectsNewData";
 
-var serverHost = "http://www.oryxtech.net/sideEffectsNewData";
+var serverHost = "https://www.oryxtech.net/sideEffectsNewData";
 
 //var currentHost = global.serverHost;
 
